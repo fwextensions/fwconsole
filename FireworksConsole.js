d7ed36cace647abc91b1ab46ea5605358c44fe07
John Dunning <fw@johndunning.com>
2012-05-01 21:02:42 -0700

    Added check for recursing too deeply. Embeded a copy of dojo's toJson function. Added extractCallerName function. Push the console messages onto an array and keep only the last 50. Worked around stupid ass processing dialog by not returning the JSON string from a function. Added countDisplay method to console.  Added option to count() to not display the current count.

--- FireworksConsole.js
+++ FireworksConsole.js
@@ -7,8 +7,8 @@
 	Email - fw@johndunning.com
 	Website - http://johndunning.com/fireworks
 
-	Release - 0.3.0 ($Revision$)
-	Last update - $Date$
+	Release - 0.3.0 ($Revision: 1.8 $)
+	Last update - $Date: 2010/05/11 19:42:17 $
 
    ======================================================================== */
 
@@ -17,10 +17,23 @@
 	To do:
 		- support %s in first string passed to console.log, etc.
 
+		- check for loops and for root object so we don't keel over when 
+			typing dojo into the console 
+
 		- support assert() methods
 			would have to assert the expresion in the caller's context
 
 	Done:
+		- add way to count without displaying, and then display the total later
+			console.count("foo", true);
+			console.countDisplay("foo");
+
+		- use dojo instead of JSON to avoid changing prototypes
+
+		- push strings onto array rather than append to a string?
+			should be faster
+			can't do it because can't return the sring from a method called by the AS?
+
 		- add console.log() API
 */
 
@@ -38,7 +51,9 @@ jdlib = jdlib || {};
 		{
 			var valueType = typeof inValue;
 
-			if (this[valueType]) {
+			if (inDepth > 10) {
+				return "<< MAX DEPTH REACHED >>";
+			} else if (this[valueType]) {
 				return this[valueType](inValue, inDepth);
 			} else {
 				return "UNKNOWN";
@@ -165,6 +180,27 @@ jdlib = jdlib || {};
 	}
 
 	
+	function extractCallerName(
+		inCaller)
+	{
+		var callerName = "";
+
+		try {
+			callerName = inCaller.NAME;
+
+			if (typeof callingFunction == "undefined" || callerName == "") {
+					// convert the function to a string, which should include
+					// the name if it's not an anonymous function
+				callerName = inCaller.toString().match(/^\s*function ([^)]+)\(/)[1];
+			}
+		} catch (exception) {
+			callerName = "";
+		}
+
+		return callerName;
+	}
+
+
 	function evaluateCode(
 		inCode)
 	{
@@ -184,184 +220,120 @@ jdlib = jdlib || {};
 
 
 	jdlib.FireworksConsole = {
-		version: "$Revision$".match(/ ([0-9.]+) /)[1],
+		version: "$Revision: 1.8 $".match(/ ([0-9.]+) /)[1],
 		evaluateCode: evaluateCode
 	};
 
 
-		// include the Crockford JSON stringify instead of relying on dojo
-	var JSON = {};
-	(function()
-	{
-		function f(n) {
-			return n < 10 ? '0' + n : n;
+		// embed a local copy of the dojo JSON library, which has been changed
+		// to not depend on any other part of dojo.  that way, we don't have to
+		// rely on external libraries.  unlike Crockford's library, the dojo
+		// implementation doesn't change the prototypes of basic types, which
+		// caused problems for the Path panel, and possibly others.
+	var dojo = {};
+
+	dojo.fromJson = function(/*String*/ json){
+		return eval("(" + json + ")"); // Object
+	}
+
+	dojo._escapeString = function(/*String*/str){
+		return ('"' + str.replace(/(["\\])/g, '\\$1') + '"').
+			replace(/[\f]/g, "\\f").replace(/[\b]/g, "\\b").replace(/[\n]/g, "\\n").
+			replace(/[\t]/g, "\\t").replace(/[\r]/g, "\\r"); // string
+	}
+
+	dojo.toJsonIndentStr = "\t";
+	dojo.toJson = function(/*Object*/ it, /*Boolean?*/ prettyPrint, /*String?*/ _indentStr){
+		if(it === undefined){
+				// dojo was incorrectly returning "undefined" here
+			return undefined;
 		}
-
-		if (typeof Date.prototype.toJSON !== 'function') {
-			Date.prototype.toJSON = function (key) {
-
-				return isFinite(this.valueOf()) ?
-					   this.getUTCFullYear()   + '-' +
-					 f(this.getUTCMonth() + 1) + '-' +
-					 f(this.getUTCDate())      + 'T' +
-					 f(this.getUTCHours())     + ':' +
-					 f(this.getUTCMinutes())   + ':' +
-					 f(this.getUTCSeconds())   + 'Z' : null;
-			};
-
-			String.prototype.toJSON =
-			Number.prototype.toJSON =
-			Boolean.prototype.toJSON = function (key) {
-				return this.valueOf();
-			};
+		var objtype = typeof it;
+		if(objtype == "number" || objtype == "boolean"){
+			return it + "";
 		}
-
-		var cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
-			escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
-			gap,
-			indent,
-			meta = {    // table of character substitutions
-				'\b': '\\b',
-				'\t': '\\t',
-				'\n': '\\n',
-				'\f': '\\f',
-				'\r': '\\r',
-				'"' : '\\"',
-				'\\': '\\\\'
-			},
-			rep;
-
-
-		function quote(string) {
-			escapable.lastIndex = 0;
-			return escapable.test(string) ?
-				'"' + string.replace(escapable, function (a) {
-					var c = meta[a];
-					return typeof c === 'string' ? c :
-						'\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
-				}) + '"' :
-				'"' + string + '"';
+		if(it === null){
+			return "null";
 		}
-
-
-		function str(key, holder) {
-			var i,          // The loop counter.
-				k,          // The member key.
-				v,          // The member value.
-				length,
-				mind = gap,
-				partial,
-				value = holder[key];
-
-			if (value && typeof value === 'object' &&
-					typeof value.toJSON === 'function') {
-				value = value.toJSON(key);
-			}
-
-			if (typeof rep === 'function') {
-				value = rep.call(holder, key, value);
-			}
-
-			switch (typeof value) {
-				case 'string':
-					return quote(value);
-
-				case 'number':
-					return isFinite(value) ? String(value) : 'null';
-
-				case 'boolean':
-				case 'null':
-					return String(value);
-
-				case 'object':
-					if (!value) {
-						return 'null';
-					}
-
-					gap += indent;
-					partial = [];
-
-					if (Object.prototype.toString.apply(value) === '[object Array]') {
-						length = value.length;
-						for (i = 0; i < length; i += 1) {
-							partial[i] = str(i, value) || 'null';
-						}
-
-						v = partial.length === 0 ? '[]' :
-							gap ? '[\n' + gap +
-									partial.join(',\n' + gap) + '\n' +
-										mind + ']' :
-								  '[' + partial.join(',') + ']';
-						gap = mind;
-						return v;
-					}
-
-					if (rep && typeof rep === 'object') {
-						length = rep.length;
-						for (i = 0; i < length; i += 1) {
-							k = rep[i];
-							if (typeof k === 'string') {
-								v = str(k, value);
-								if (v) {
-									partial.push(quote(k) + (gap ? ': ' : ':') + v);
-								}
-							}
-						}
-					} else {
-						for (k in value) {
-							if (Object.hasOwnProperty.call(value, k)) {
-								v = str(k, value);
-								if (v) {
-									partial.push(quote(k) + (gap ? ': ' : ':') + v);
-								}
-							}
-						}
-					}
-
-					v = partial.length === 0 ? '{}' :
-						gap ? '{\n' + gap + partial.join(',\n' + gap) + '\n' +
-								mind + '}' : '{' + partial.join(',') + '}';
-					gap = mind;
-					return v;
+		if(typeof it == "string" || it instanceof String){
+			return dojo._escapeString(it);
+		}
+		// recurse
+		var recurse = arguments.callee;
+		// short-circuit for objects that support "json" serialization
+		// if they return "self" then just pass-through...
+		var newObj;
+		_indentStr = _indentStr || "";
+		var nextIndent = prettyPrint ? _indentStr + dojo.toJsonIndentStr : "";
+		var tf = it.__json__||it.json;
+		if(typeof tf == "function"){
+			newObj = tf.call(it);
+			if(it !== newObj){
+				return recurse(newObj, prettyPrint, nextIndent);
 			}
 		}
+		if(it.nodeType && it.cloneNode){ // isNode
+			// we can't seriailize DOM nodes as regular objects because they have cycles
+			// DOM nodes could be serialized with something like outerHTML, but
+			// that can be provided by users in the form of .json or .__json__ function.
+			throw new Error("Can't serialize DOM nodes");
+		}
 
-		JSON.stringify = function (value, replacer, space) {
-			var i;
-			gap = '';
-			indent = '';
+		var sep = prettyPrint ? " " : "";
+		var newLine = prettyPrint ? "\n" : "";
 
-			if (typeof space === 'number') {
-				for (i = 0; i < space; i += 1) {
-					indent += ' ';
+		// array
+		if(it && it instanceof Array){
+			var res = [];
+			for (var i = 0, len = it.length; i < len; i++) {
+				var val = recurse(it[i], prettyPrint, nextIndent);
+				if(typeof val != "string"){
+						// dojo was incorrectly returning "undefined" here
+					val = undefined;
 				}
-			} else if (typeof space === 'string') {
-				indent = space;
+				res[i] = newLine + nextIndent + val;
 			}
-
-			rep = replacer;
-			if (replacer && typeof replacer !== 'function' &&
-					(typeof replacer !== 'object' ||
-					 typeof replacer.length !== 'number')) {
-				throw new Error('JSON.stringify');
+			return "[" + res.join("," + sep) + newLine + _indentStr + "]";
+		}
+		if(objtype == "function"){
+			return null; // null
+		}
+		// generic object code path
+		var output = [], key;
+		for(key in it){
+			var keyStr, val;
+			if(typeof key == "number"){
+				keyStr = '"' + key + '"';
+			}else if(typeof key == "string"){
+				keyStr = dojo._escapeString(key);
+			}else{
+				// skip non-string or number keys
+				continue;
 			}
+			val = recurse(it[key], prettyPrint, nextIndent);
+			if(typeof val != "string"){
+				// skip non-serializable values
+				continue;
+			}
+			// FIXME: use += on Moz!!
+			//	 MOW NOTE: using += is a pain because you have to account for the dangling comma...
+			output.push(newLine + nextIndent + keyStr + ":" + sep + val);
+		}
+		return "{" + output.join("," + sep) + newLine + _indentStr + "}"; // String
+	}
 
-			return str('', {'': value});
-		};
-	})();
+
+	var _counts = {},
+		_timers = {};
 
 
-	var _counts = {};
-	var _timers = {};
-
-
-	function _now()
+	function now()
 	{
 		return (new Date()).getTime();
 	}
 
 
-	function _addLogEntry(
+	function addLogEntry(
 		inType,
 		inCaller)
 	{
@@ -382,59 +354,102 @@ jdlib = jdlib || {};
 				s.push(__StringFormatter__.format(variant));
 			}
 		}
-		
-		var entryJSON = JSON.stringify({
+
+			// due to the annoying-as-fuck modal "processing command" dialog that
+			// will appear on top of FW and require a force-quit if the call stack
+			// gets more than one or two levels deep, we can't delay the conversion
+			// of the log entries to JSON until the console polls for the latest
+			// entries.  so we have to convert the parameters to a JSON string
+			// now, and push the string onto an array.  ffs.
+		console._logEntries.push(dojo.toJson({
 			type: inType,
 			text: s.join(" "),
-			caller: inCaller,
-			time: _now()
-		});
+			caller: extractCallerName(inCaller),
+			time: now()
+		}));
 
-		if (console._logEntriesJSON.length > 75000) {
-				// the console isn't running and picking up these log entries, so
-				// clear the entries to keep them from growing too long
-			console._logEntriesJSON = "";
-		} else if (console._logEntriesJSON.length > 0) {
-			console._logEntriesJSON += ",";
-		}
-
-		console._logEntriesJSON += entryJSON;
+			// keep only the last 50 log entries, in case the console panel
+			// isn't picking them up
+		console._logEntries = console._logEntries.slice(-50);
 	}
 
 
 	console = {
-		_logEntriesJSON: "",
-
 			// we need to set this so dojo doesn't wipe out the console object
 			// if it loads after us
 		firebug: true,
 
+			// this array stores the JSON strings until the panel wants them
+		_logEntries: [],
+		
+			// this string temporarily holds the JSON string version of the
+			// _logEntries array
+		_logEntriesJSON: "",
+
+
+		_prepLogEntriesJSON: function()
+		{
+			var entries = this._logEntries;
+
+				// clear the array, so that we don't redisplay old entries
+			this._logEntries = [];
+
+				// convert the array to JSON and store it on a property of
+				// console.  due to the motherless-whore processing dialog, we
+				// can't simply return this string to the panel.  instead, the
+				// panel has to call this method and then access
+				// console._logEntriesJSON to get the JSON string.
+			this._logEntriesJSON = "[" + entries.join(",") + "]";
+		},
+
+
 		time: function(
 			inTimerName)
 		{
-			_timers[inTimerName] = _now()
+			_timers[inTimerName] = now()
 		},
+
 
 		timeEnd: function(
 			inTimerName)
 		{
 			if (_timers[inTimerName]) {
-				var delta = _now() - _timers[inTimerName];
-				console.log(inTimerName + ":", delta / 1000, "sec");
+				var delta = now() - _timers[inTimerName];
+				addLogEntry("log", arguments.callee.caller, inTimerName + ":", delta / 1000, "sec");
 				delete _timers[inTimerName];
 			}
 		},
 
+
 		count: function(
-			inCountName)
+			inCountName,
+			inSuppressDisplay)
 		{
 			if (!_counts[inCountName]) {
 				_counts[inCountName] = 0;
 			}
 
 			_counts[inCountName]++;
-			console.log(inCountName + ":", _counts[inCountName]);
+
+			if (!inSuppressDisplay) {
+				addLogEntry("log", arguments.callee.caller, inCountName + ":", _counts[inCountName]);
+			}
 		},
+
+
+		countDisplay: function(
+			inCountName,
+			inReset)
+		{
+			if (_counts[inCountName]) {
+				addLogEntry("log", arguments.callee.caller, inCountName + ":", _counts[inCountName]);
+
+				if (inReset) {
+					delete _counts[inCountName];
+				}
+			}
+		},
+
 
 		countReset: function(
 			inCountName)
@@ -442,33 +457,21 @@ jdlib = jdlib || {};
 			delete _counts[inCountName];
 		},
 
+
 		clear: function()
 		{
 			_logEntriesJSON = "";
 		}
 	};
 
-		// create functions that pass different types to _addLogEntry
+		// create functions that pass different types to addLogEntry
 	for (var functionName in { log:1, warn:1, info:1, debug:1, error:1 }) {
 		console[functionName] = (function(inFunctionName) {
 			return function()
 				{
-						// we have to capture the calling function's name here,
-						// rather than in _addLogEntry, since that function sees
-						// its caller as apply()
-					var callingFunction = "";
-
-					try {
-						callingFunction = arguments.callee.caller.NAME;
-
-						if (typeof callingFunction == "undefined" || callingFunction == "") {
-							callingFunction = arguments.callee.caller.toString().match(/^\s*function ([^)]+)\(/)[1];
-						}
-					} catch (exception) {
-						callingFunction = "";
-					}
-					
-					_addLogEntry.apply(this, [inFunctionName, callingFunction].concat(arguments));
+						// pass our caller in so that addLogEntry can extract
+						// its name
+					addLogEntry.apply(this, [inFunctionName, arguments.callee.caller].concat(arguments));
 				};
 		})(functionName);
 	}
