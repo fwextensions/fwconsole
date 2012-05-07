dabfda92ae7a6fa97b27aeb39ce9eea4456a34ff
John Dunning <fw@johndunning.com>
2012-05-07 10:43:13 -0700

    Commented out moves to old FW directories.

--- _build.bat
+++ _build.bat
@@ -5,11 +5,13 @@ REM SET OPTS=-use-network=true -output=C:\Projects\LCD Frame\Slideshow\resources
 REM SET OPTS=-use-network=true 
 
 	rem use 200x150 as the default size of the swf, since the app's size is set to 100% in the mxml
-SET OPTS=-use-network=true -warn-no-type-decl=false -library-path+=assets -library-path+=..\lib -source-path+=..\src -theme assets\Ice.css -keep-generated-actionscript=true -incremental=true -default-size 200 150
+SET OPTS=-use-network=true -warn-no-type-decl=false -library-path+=assets -library-path+=..\lib -source-path+=..\src -keep-generated-actionscript=true -incremental=true -default-size 200 150
+rem SET OPTS=-use-network=true -warn-no-type-decl=false -library-path+=assets -library-path+=..\lib -source-path+=..\src -theme assets\Ice.css -keep-generated-actionscript=true -incremental=true -default-size 200 150
 
 "C:\Software\Development\Flex\Flex 3\flex_sdk_3.0.0.477\bin\mxmlc" %OPTS% -output FireworksConsole.swf FireworksConsole.mxml
 
-cp "FireworksConsole.swf" "C:\Program Files\Adobe\Adobe Fireworks CS5\Configuration\Command Panels\Fireworks Console.swf"
+rem copy "FireworksConsole.swf" "C:\Program Files\Adobe\Adobe Fireworks CS5\Configuration\Command Panels\Fireworks Console.swf"
+rem copy "FireworksConsole.swf" "C:\Program Files\Adobe\Adobe Fireworks CS5.1\Configuration\Command Panels\Fireworks Console.swf"
 rem cp "FireworksConsole.swf" "C:\Program Files\Adobe\Adobe Fireworks CS4\Configuration\Command Panels\Fireworks Console.swf"
 move "FireworksConsole.swf" "C:\Program Files\Adobe\Adobe Fireworks CS3\Configuration\Command Panels\Fireworks Console.swf"
 
